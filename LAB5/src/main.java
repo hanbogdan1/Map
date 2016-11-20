b5ca5bed8192b785e669b5b2f437ccdf6c757e00
Keep Coding Izi Life <ureniucbogdan@gmail.com>
2016-11-20 13:33:08 +0200

    test
    doar untest

--- LAB5/src/main.java
+++ LAB5/src/main.java
@@ -1,11 +1,23 @@
 import UserInterface.*;
-public class main {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-	    public static void main(String args[]){
-	        UI ui = new UI(){};
-	        
-	        ui.start();
-	    }
-	
-//da ba
+
+public class main extends Application{
+    public static void main(String argv[]){
+        launch(argv);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+//        Scene scene = new Scene(new Parent(),500,500);
+//        primaryStage.setScene(scene);
+//        primaryStage.setTitle("Book Repository");
+//        primaryStage.show();
+      
+    }
 }
