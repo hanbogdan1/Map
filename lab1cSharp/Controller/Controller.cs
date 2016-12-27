d63d21ccb34e1da1d678e59282c5346a71b501c5
Keep Coding Izi Life <ureniucbogdan@gmail.com>
2016-12-27 21:40:09 +0200

    dsa

--- lab1cSharp/Controller/Controller.cs
+++ lab1cSharp/Controller/Controller.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using lab1cSharp.Repository;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace lab1cSharp.Controller
 {
     public class Controller
     {
+        private PostRepository PostRepo;
+        private SarcinaRepository SarcinaRepo;
+        private FisaRepository FisaRepo;
+
     }
 }
