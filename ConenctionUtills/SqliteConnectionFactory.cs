730e6ea6eb82fb7a59582593d1e794803151afed
Keep Coding Izi Life <ureniucbogdan@gmail.com>
2017-03-07 08:52:15 +0200

    rezovlare database connection

---
+++ ConenctionUtills/SqliteConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+
+namespace ConenctionUtills
+{
+    class SqliteConnectionFactory:ConnectionFactory
+    {
+
+        public override IDbConnection createConnection()
+        {           
+            String connectionString = "Data Source=database.db;Version=3.1.1";
+            return new SQLiteConnection(connectionString);           
+        }
+    }
+}
